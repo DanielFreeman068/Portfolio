defb38090a99f959c56aab764844d1fa0df752a5
DanielFreeman068 <141285622+DanielFreeman068@users.noreply.github.com>
2024-04-19 15:03:37 -0700

    Add files via upload

--- projects.js
+++ projects.js
@@ -7,43 +7,31 @@ box.addEventListener('mouseenter', () => {
         box.classList.remove('carouselHover');
     },2000);
 });
-//pops up tic tac toe info
-const infoOne = document.getElementById('one');
-const displayOne = document.getElementById('uno');
 
-infoOne.addEventListener('mouseenter', () => {
-    displayOne.classList.add('magic');
-    setTimeout(() => {
-        displayOne.classList.remove('magic');
-    }, 2000);
-});
-//pops up card memory info
-const infoTwo = document.getElementById('two');
-const displayTwo = document.getElementById('dos');
-
-infoTwo.addEventListener('mouseenter', () => {
-    displayTwo.classList.add('magic');
-    setTimeout(() => {
-        displayTwo.classList.remove('magic');
-    }, 2000);
-});
-//pops up photo gallery info
-const infoThree = document.getElementById('three');
-const displayThree = document.getElementById('tres');
-
-infoThree.addEventListener('mouseenter', () => {
-    displayThree.classList.add('magic');
-    setTimeout(() => {
-        displayThree.classList.remove('magic');
-    }, 2000);
-});
-//pops up calculator info
-const infoFour = document.getElementById('four');
-const displayFour = document.getElementById('quatro');
-
-infoFour.addEventListener('mouseenter', () => {
-    displayFour.classList.add('magic');
-    setTimeout(() => {
-        displayFour.classList.remove('magic');
-    }, 2000);
+//Pops up the information
+$(document).ready(function(){
+    $("#one").mouseenter(function(){
+        $("#uno").slideDown(1000); // Slide down when mouse enters
+    });
+    $("#uno").mouseleave(function(){
+        $("#uno").slideUp(1000); // Slide up when mouse leaves
+    });
+    $("#two").mouseenter(function(){
+        $("#dos").slideDown(1000); // Slide down when mouse enters
+    });
+    $("#dos").mouseleave(function(){
+        $("#dos").slideUp(1000); // Slide up when mouse leaves
+    });
+    $("#three").mouseenter(function(){
+        $("#tres").slideDown(1000).setTimeout(5000).slideUp(1000); // Slide down when mouse enters
+    });
+    $("#tres").mouseleave(function(){
+        $("#tres").slideUp(1000); // Slide up when mouse leaves
+    });
+    $("#four").mouseenter(function(){
+        $("#quatro").slideDown(1000).setTimeout(5000).slideUp(1000); // Slide down when mouse enters
+    });
+    $("#quatro").mouseleave(function(){
+        $("#quatro").slideUp(1000); // Slide up when mouse leaves
+    });
 });
